57749671e8df49941be119d4f7055d882d1528cb
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2025-05-23 12:56:19 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 
+import PageWrapper from "@/components/PageWrapper";
 import React from "react";
-
+import { ProductsProvider } from "./context/productsContext";
+import App from "./App";
 
 export default function Home() {
   return (
     <div className="grid min-h-screen">
       <main className="flex flex-col gap-[32px] items-center justify-center">
         <h1>Hello, this is a test!</h1>;
-        {/* <PageWrapper>
+        <PageWrapper>
           <ProductsProvider>
             <App />
           </ProductsProvider>
-        </PageWrapper> */}
+        </PageWrapper>
       </main>
     </div>
   );
